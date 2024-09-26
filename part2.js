6cd6bf2e932d10e87f523fb943647257052ab55a
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-09-25 18:14:06 -0700

    Put array inside 'data' variable

--- part2.js
+++ part2.js
@@ -1,4 +1,4 @@
-[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+const data = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
     { id: "48", name: "Barry", occupation: "Runner", age: "25" },
     { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
     { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
